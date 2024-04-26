d52fb63403806f0343de0a441e7efa17cb757883
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-04-26 17:28:05 +0300

    feat: add message decoding functionality to Message class

--- message.py
+++ message.py
@@ -1,2 +1,9 @@
 class Message:
-    pass
+    # Message format <type>{:}<content>
+    def __init__(self, data, *args, **kwargs):
+        self.data = data
+        self.type = None
+        self.content = None
+        self.decode()
+    def decode(self):
+        self.type, self.content = self.data.split("{:}")
