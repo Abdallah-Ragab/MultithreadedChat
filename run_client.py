4141f75253fed942857169084c35b02eecfbbd7a
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-04-26 21:38:34 +0300

    refactor: Update run_client.py to fix syntax error in client instantiation

--- run_client.py
+++ run_client.py
@@ -7,7 +7,7 @@ from client import Client
 names = ["Abdallah", "Mohamed", "Ahmed", "Ali", "Omar", "Khaled", "Youssef", "Hassan", "Hussein", "Sayed", "Khalil", "Khalid", "Mona", "Nada", "Nour", "Noura", "Noha", "Nada", "Nadia", "Naglaa", "Nahla", "Nahed", "Nahla", "Naila", "Najla", "Najat", "Najwa", "Nariman", "Nashwa",]
 
 
-client = Client(username=random.choice(names)
+client = Client(username=random.choice(names))
 def msg():
     time.sleep(5)
     client.send("Hello, World!")
