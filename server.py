36c3fdf49511cc358b91aa07aedc68e165f3cc6f
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-04-26 15:42:01 +0300

    created server class which listens and accepts connections

---
+++ server.py
@@ -0,0 +1,41 @@
+import socket
+import logging
+from threading import Thread
+
+
+class Server():
+
+    Clients = []
+    host = "127.0.0.1"
+    port = 8080
+    logger = logging.getLogger()
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def run(self, *args, **kwargs):
+        self.Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.Socket.bind(self.host, self.port)
+        self.logger.info("[ + ] Server Started.")
+        self.listen_for_connections()
+
+    def listen_for_connections(self):
+        while True:
+            self.Socket.listen(1)
+            connection, address = self.Socket.accept()
+            client = Client(
+                id = len(self.Clients),
+                connection=connection,
+                address=address
+            )
+
+
+class Client:
+
+    def __init__(self, id, connection, address, *args, **kwargs):
+        pass
+
+class Connection(Thread):
+
+    def __init__(self, client,  *args, **kwargs):
+        super(Connection, self).__init__(*args, **kwargs)
