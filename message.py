821bc7c6632a28d51c6b482897a43eee79b9f48b
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-04-26 17:45:06 +0300

    refactor: update message format in Message class

--- message.py
+++ message.py
@@ -1,9 +1,26 @@
 class Message:
-    # Message format <type>{:}<content>
-    def __init__(self, data, *args, **kwargs):
+    # Message format <type>:::<content>
+    separator = ":::"
+
+    def __init__(self, data = None, msg_type = None, content = None, *args, **kwargs):
         self.data = data
-        self.type = None
-        self.content = None
-        self.decode()
-    def decode(self):
-        self.type, self.content = self.data.split("{:}")
+        self.type = msg_type
+        self.content = content
+
+    @classmethod
+    def decode(data):
+        msg = Message(data)
+        msg.type, msg.content = msg.data.split(Message.separator)
+        return msg
+
+    @classmethod
+    def encode(msg_type, content):
+        msg = Message(msg_type, content)
+        msg.data = f"{msg.type}{Message.separator}{msg.content}"
+        return msg.data
+
+    def __str__(self):
+        return self.data
+
+    def __repr__(self):
+        return self.data
