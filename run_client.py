aef59c5d2659d6967b090ebad5e28043bc908890
Abdallah Ragab <abdallahsameh1140@gmail.com>
2024-04-26 20:47:14 +0300

    refactor: Add lazy loading for images in run_client.py

--- run_client.py
+++ run_client.py
@@ -1,5 +1,12 @@
+import threading
+import time
 from client import Client
 
 client = Client()
+def msg():
+    time.sleep(5)
+    client.send("Hello, World!")
+
+threading.Thread(target=msg).start()
+
 client.connect()
-client.send("Hello, World!")
